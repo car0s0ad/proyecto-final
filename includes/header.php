0ecd5b3516b3722f8a4fa07c0bc189556f0c80e2
ili93 <76755473+ili93@users.noreply.github.com>
2021-02-15 20:12:35 -0300

    agrego el index

---
+++ includes/header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <title>G7 inmobiliaria</title>
+        <!--BOOTSTRAP-->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+        <link rel="stylesheet" href="estilos.css">
+    </head>
+    <header> 
+            <nav class="navbar navbar-expand-lg navbar-light bg-info">
+                    <a> <img class="logo" src=./recursos/logo.png width=97 height=35 alt="logo"> </a>
+                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div class="navbar-nav ml-auto">
+                        <a class="nav-item nav-link" href="#">Propiedades</a>
+                        <a class="nav-item nav-link" href="#">Publicar mi inmueble</a>
+                        <a class="nav-item nav-link" href="#">Sobre nuestro proyecto</a>
+                        <a class="nav-item nav-link" href="#">Contacto</a>
+                       
+                    </div>
+                </div>
+            </nav> 
+        </header>
+</html>
---
+++ index.php
@@ -0,0 +1,42 @@
+<?php include("includes/header.php") ?>
+    <body>
+        <main role="main"></main>
+         <section class="col-" id=fondo> 
+                      <div class="container-fluid">
+                       <div class="row">
+
+                      </div>
+        
+              </div>
+            </div>
+          </section>
+                
+
+         
+         
+         <section class="col- py-5">
+            <div class="container-fluid container-md py-5">  
+               <div class="row align-items-center">
+                <h1>Pasos del proceso</h1>
+                <ul>
+                    <li>Inscripción</li>
+                    <li>Test Online (TERP + Test Técnico Múltiple Choice)</li>
+                    <li>Dinámica Grupal Online</li>
+                    <li>Hackaton: con 24 a 30 candidatos aprox</li>
+                </ul>
+            </div>
+        </section>
+
+
+      
+
+
+
+
+
+
+
+
+   
+    </body>
+    <?php include("includes/footer.php") ?>
