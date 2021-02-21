31bf4336a22c5c1a2b26d23d15f6c338b30fe6b2
Carolina Dehle <carodehle@gmail.com>
2021-02-20 21:12:19 -0300

    botones de busqueda

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php include("includes/header.php") ?>
+<?php include("includes/bootstrap.html") ?>
     <html class="h-100">
     <body class="h-100">
         <main role="main"></main>        
@@ -8,27 +9,74 @@
                <div class="row align-items-center col-md-6">
                 <h1>Encontrá</h1> <br>
                 <br><h1>tu nuevo hogar</h1>
-
             </div>
         </section>
-      <h5>espacio</h5>
-      <h5>espacio</h5>
-      <h5>espacio</h5>
-      <h5>espacio</h5>
-      <h5>espacio</h5>
-      <h5>espacio</h5>
-      <h5>espacio</h5>
-      <h5>espacio</h5>
-      <h5>espacio</h5>
-      <h5>espacio</h5>
-      <h5>espacio</h5>
-      <h5>espacio</h5>
-      <h5>espacio</h5>
-      <h5>espacio</h5>
-      <h5>espacio</h5>
-      <h5>espacio</h5>
-      <h5>espacio</h5>
-      <h5>espacio</h5>
+
+<section>
+  <div class="row">
+    <div class="col-md-2">
+      <select class="form-select">
+        <option selected>Operación</option>
+        <option value="1">Alquiler</option>
+        <option value="2">Venta</option>
+        <option value="3">Alquiler temporal</option>
+      </select>
+    </div>
+  </div>
+<br>
+  <div class="row">
+    <div class="col-md-2">
+      <select class="form-select">
+        <option selected> Tipo de Inmueble</option>
+        <option value="1">Cualquier tipo de propiedad</option>
+        <option value="2">Casa</option>
+        <option value="3">Departamento</option>
+        <option value="4">Local comercial</option>
+        <option value="5">Terreno</option>
+        <option value="6">Oficina/Consultorio</option>
+        <option value="7">Depósito</option>
+        <option value="8">Quinta</option>
+      </select>
+    </div>
+  </div>
+  <br>
+  <div class="row">
+    <div class="col-md-2">
+      <select class="form-select">
+        <option selected>Ambientes</option>
+        <option value="1">Monoambiente</option>
+        <option value="2">2 ambientes</option>
+        <option value="3">3 ambientes</option>
+        <option value="4">4 ambientes</option>
+        <option value="5">5 ambientes</option>
+      </select>
+    </div>
+  </div>
+</section>
+ 
+
+   
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
     
     </body>
     <?php include("includes/footer.php") ?>
