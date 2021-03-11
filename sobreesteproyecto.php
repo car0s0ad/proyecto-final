efa595624ac74ebbf7e304cb8c5a06d438504fa8
ili93 <76755473+ili93@users.noreply.github.com>
2021-03-10 22:59:52 -0300

    acomodo texto

--- sobreesteproyecto.php
+++ sobreesteproyecto.php
@@ -6,15 +6,20 @@
         <main role="main"></main>        
          
          <section class="col- py-5">
-            <div class="container-fluid container-md py-5">  
+            <div class="container-fluid container-md">  
                <div class="row align-items-center col-md-6">
                 <h1 class="neon">Sobre este proyecto</h1>
             </div>
-        </section>
-<H5>Somos grupo 7, un grupo de estudiantes que busca mejorar la experiencia de quienes desean radicarse en nuestra ciudad. 
-Conocemos muy bien las dificultades que se les presentan a los jóvenes que buscan su primer hogar en Posadas, ya sea para venir a estudiar o para independizarse luego de encontrar su primer trabajo. Por eso, este proyecto busca facilitar el acceso a la vivienda joven, acercando propuestas menos restrictivas, a las cuales puedan acceder sin problemas tanto personas solas como parejas y nuevas familias con o sin niños, mascotas… o como sea que esté constituida. 
-Queremos generar un lugar común en el que tanto anunciantes como posibles inquilinos se encuentren de manera sencilla, así como también mejorar el nivel de los anuncios, dejando a disposición toda la información que el usuario necesita.</H5>
-
+        </section class="sobrenosotros">
+        <div class="row offset-4 col-8">
+        <div class="container">
+        <div class="container-fluid align-items-end">
+            <H5>Somos grupo 7, un grupo de estudiantes que busca mejorar la experiencia de quienes desean radicarse en nuestra ciudad. 
+             Conocemos muy bien las dificultades que se les presentan a los jóvenes que buscan su primer hogar en Posadas, ya sea para venir a estudiar o para independizarse luego de encontrar su primer trabajo. Por eso, este proyecto busca facilitar el acceso a la vivienda joven, acercando propuestas menos restrictivas, a las cuales puedan acceder sin problemas tanto personas solas como parejas y nuevas familias con o sin niños, mascotas… o como sea que esté constituida. 
+             Queremos generar un lugar común en el que tanto anunciantes como posibles inquilinos se encuentren de manera sencilla, así como también mejorar el nivel de los anuncios, dejando a disposición toda la información que el usuario necesita.</H5>
+        </div>
+        </div>
+        </div>
 <br>
 <br>
 <br>
