cb21699408a56215fa8d74d2c8fa4ff3911a8b38
ili93 <76755473+ili93@users.noreply.github.com>
2021-03-13 14:59:57 -0300

    cambios en el logo de linkedin

--- contacto.php
+++ contacto.php
@@ -18,27 +18,27 @@
                <h5 class="align-self-center">Antonella Pereira</h5>
                <br>
                <a href="mailto:grupo7@gmail.com"> <img class="logo mx-2"  src="./recursos/ICONOS/mail1.png" href="#" width=32 height=22 alt="logo"> </a>
-               <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/ileana-marinoni-18458b1a4/"> <img class="logo mx-2" src="./recursos/ICONOS/in1.png" href="#" width=30 height=30 alt="logo"> </a>
+               <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/ileana-marinoni-18458b1a4/"> <img class="logo mx-2" src="./recursos/ICONOS/in2.png" href="#" width=30 height=30 alt="logo"> </a>
              </div>
              
              <div class="d-flex flex-row ml-5 py-2">
                <h5 class="align-self-center">Carolina Dehle</h5>
                <br>
                <a href="mailto:grupo7@gmail.com"> <img class="logo mx-2"  src="./recursos/ICONOS/mail1.png" href="#" width=32 height=22 alt="logo"> </a>
-               <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/ileana-marinoni-18458b1a4/"> <img class="logo mx-2" src="./recursos/ICONOS/in1.png" href="#" width=30 height=30 alt="logo"> </a>
+               <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/ileana-marinoni-18458b1a4/"> <img class="logo mx-2" src="./recursos/ICONOS/in2.png" href="#" width=30 height=30 alt="logo"> </a>
              </div>
              
              <div class="d-flex flex-row ml-5 py-2">
                <h5 class="align-self-center">Ileana Marinoni</h5>
                <br>
                <a href="mailto:grupo7@gmail.com"> <img class="logo mx-2"  src="./recursos/ICONOS/mail1.png" href="#" width=32 height=22 alt="logo"> </a>
-               <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/ileana-marinoni-18458b1a4/"> <img class="logo mx-2" src="./recursos/ICONOS/in1.png" href="#" width=30 height=30 alt="logo"> </a>
+               <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/ileana-marinoni-18458b1a4/"> <img class="logo mx-2" src="./recursos/ICONOS/in2.png" href="#" width=30 height=30 alt="logo"> </a>
              </div>
             
              <div class="d-flex flex-row ml-5 pt-2">
                <h5 class="align-self-center">Leandro Portillo</h5>
                <a href="mailto:grupo7@gmail.com"> <img class="logo mx-2"  src="./recursos/ICONOS/mail1.png" href="#" width=32 height=22 alt="logo"> </a>
-               <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/ileana-marinoni-18458b1a4/"> <img class="logo mx-2" src="./recursos/ICONOS/in1.png" href="#" width=30 height=30 alt="logo"> </a>
+               <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/ileana-marinoni-18458b1a4/"> <img class="logo mx-2" src="./recursos/ICONOS/in2.png" href="#" width=30 height=30 alt="logo"> </a>
                
                
              </div>
