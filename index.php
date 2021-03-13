d15466638a25d2abd882f3b0bc9ca288c3f39814
leanjpg <lean.jpg@gmail.com>
2021-03-12 21:34:05 -0300

    subo el archivo de buscarinmueble, y edito el boton de buscar

--- index.php
+++ index.php
@@ -57,7 +57,7 @@
       <br>
       <div class="row col-5">
       <div class="container-fluid justify-content-center">
-        <input type="submit" value="Buscar" href="#"></a>
+       <a href="buscarinmueble.php" class="btn btn-primary"> Buscar</a>
       </div>
       </div>
       </div>
