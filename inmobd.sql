6bd25e5f847a24f11ecd7b6ccf452bd39d9d7d65
Carolina Dehle <carodehle@gmail.com>
2021-03-18 15:09:44 -0300

    commit

--- inmobd.sql
+++ inmobd.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 18-03-2021 a las 00:54:59
+-- Tiempo de generación: 18-03-2021 a las 19:08:43
 -- Versión del servidor: 10.4.18-MariaDB
 -- Versión de PHP: 8.0.3
 
@@ -41,15 +41,22 @@ CREATE TABLE `propiedades` (
   `direccion` varchar(200) DEFAULT NULL,
   `propiedad_img` varchar(200) DEFAULT NULL,
   `otros_detalles` text DEFAULT NULL,
-  `atributos` varchar(255) NOT NULL
+  `cochera` varchar(255) NOT NULL,
+  `balcon` varchar(255) NOT NULL,
+  `ninos` varchar(255) NOT NULL,
+  `mascotas` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Volcado de datos para la tabla `propiedades`
 --
 
-INSERT INTO `propiedades` (`propiedad_id`, `propiedad_titulo`, `tipo_anunciante`, `nombre_anunciante`, `tipo_inmueble`, `operacion`, `metros_cuadrados`, `ambientes`, `precio`, `expensas`, `direccion`, `propiedad_img`, `otros_detalles`, `atributos`) VALUES
-(2, '', 'Anunciante', '', 'Tipo de Inmueble', 'Operación', 0, 0, 0, 0, '', NULL, 'cochera,mascotas', 'Escribe aquí otros detalles');
+INSERT INTO `propiedades` (`propiedad_id`, `propiedad_titulo`, `tipo_anunciante`, `nombre_anunciante`, `tipo_inmueble`, `operacion`, `metros_cuadrados`, `ambientes`, `precio`, `expensas`, `direccion`, `propiedad_img`, `otros_detalles`, `cochera`, `balcon`, `ninos`, `mascotas`) VALUES
+(2, '', 'Anunciante', '', 'Tipo de Inmueble', 'Operación', 0, 0, 0, 0, '', NULL, 'cochera,mascotas', 'Escribe aquí otros detalles', '', '', ''),
+(3, 'Casad', '1', 'Carolina', '3', '1', 12, 2, 12, 12, 'A Guacurari 340', NULL, 'Escribe aquí otros detalles', '', '', '', ''),
+(4, 'Casa', 'Dueño directo', 'Carolina', 'Casa', 'Alquiler', 12, 2, 12, 12, 'A Guacurari 340', NULL, 'Escribe aquí otros detallesdasdasd', '', '', '', ''),
+(5, 'Casa', 'Dueño directo', 'Carolina', 'Casa', 'Alquiler', 12, 2, 12, 1, 'A Guacurari 340', NULL, 'Escribe aquí otros detalles', '', '', '', 'Se permiten mascotas'),
+(6, 'Casa', 'Dueño directo', 'Carolina', 'Casa', 'Alquiler', 50, 0, 12, 1, 'A Guacurari 340', NULL, 'Escribe aquí otros detalles', 'No tiene cochera', 'No tiene balcón', 'Se permiten niños', 'No se permiten mascotas');
 
 --
 -- Índices para tablas volcadas
@@ -69,7 +76,7 @@ ALTER TABLE `propiedades`
 -- AUTO_INCREMENT de la tabla `propiedades`
 --
 ALTER TABLE `propiedades`
-  MODIFY `propiedad_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `propiedad_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
