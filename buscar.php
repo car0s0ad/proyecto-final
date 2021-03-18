7c9a330a7cd03d5b64cc19358cc1b17259064a3e
leanjpg <lean.jpg@gmail.com>
2021-03-18 03:37:18 -0300

    hice que el buscador filtre los resultados

---
+++ buscar.php
@@ -0,0 +1,63 @@
+<?php
+	$servername = "localhost";
+    $username = "root";
+  	$password = "";
+  	$dbname = "inmobd";
+
+	$conn = new mysqli($servername, $username, $password, $dbname);
+      if($conn->connect_error){
+        die("Conexión fallida: ".$conn->connect_error);
+      }
+
+    $salida = "";
+
+    $query = "SELECT * FROM inmueble ORDER By inmueble_id LIMIT 25";
+
+    if (isset($_POST['consulta'])) {
+    	$q = $conn->real_escape_string($_POST['consulta']);
+    	$query = "SELECT * FROM inmueble WHERE inmueble_id LIKE '%$q%' LIKE '%$q%' OR propiedad_titulo LIKE '%$q%' OR tipo_anunciante LIKE '%$q%' OR tipo_inmueble LIKE '$q' ";
+    }
+
+    $resultado = $conn->query($query);
+
+	
+
+    if ($resultado->num_rows>0) {
+    	$salida.="<table border=1 class='tabla_datos'>
+    			<thead>
+    				<tr id='titulo'>
+    					<td>ID</td>
+    					<td>Titulo</td>
+    					<td>Tipo de Anunciante</td>
+    					<td>Tipo de Inmueble</td>
+    					<td>Operacion</td>
+    				</tr>
+
+    			</thead>
+    			
+
+    	<tbody>";
+
+    	while ($fila = $resultado->fetch_assoc()) {
+    		$salida.="<tr>
+    					<td>".$fila['inmueble_id']."</td>
+    					<td>".$fila['propiedad_titulo']."</td>
+    					<td>".$fila['tipo_anunciante']."</td>
+    					<td>".$fila['tipo_inmueble']."</td>
+    					<td>".$fila['operacion']."</td>
+    				</tr>";
+
+    	}
+    	$salida.="</tbody></table>";
+    }else{
+    	$salida.="NO HAY DATOS :(";
+    }
+
+
+    echo $salida;
+
+    $conn->close();
+
+
+
+?>
--- resultados.php
+++ resultados.php
@@ -1,17 +1,24 @@
 <?php include("includes/header.php") ?>
 <?php include("includes/bootstrap.html") ?>
     <html class="h-100">
-    <body>
+<body>
         <main role="main"></main>        
   
-         <section class="col- pt-5">
-            <div class="container-fluid container-md pt-5">  
-               <div class="row align-items-center ml-2 col-md-5">
-                <h1>Buscador</h1>
-                
-            </div>
-        </section>
+    <section class="principal">
 
+        <h1>BUSQUEDA DE INMUEBLES</h1>
+
+        <div class="formulario">
+            <label for="caja_busqueda">Buscar</label>
+            <input type="text" name="caja_busqueda" id="caja_busqueda"></input>
+
+    
+        </div>
+
+        <div id="datos"></div>
+
+
+    </section>
 
   
 
@@ -28,7 +35,8 @@
 <br>
 <br>
  
-    
-    </body>
-    <?php include("includes/footer.php") ?>
-    </html>
+    <script type="text/javascript" src="js/jquery.min.js"></script>
+    <script type="text/javascript" src="js/main.js"></script>
+</body>
+<?php include("includes/footer.php") ?>
+</html>
